32b818ecbcdee6f83328c77c8be9e12f9618bb65
spikemarina152@gmail.com <spikemarina152@gmail.com>
2021-12-08 20:07:28 +0200

    z

--- CinemaTicketBookingSystem/Program.cs
+++ CinemaTicketBookingSystem/Program.cs
@@ -6,7 +6,7 @@ namespace CinemaTicketBookingSystem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Privet!");
         }
     }
 }
