940bc82c013339239b7d016c9f0ce237ddef2723
julijaastratova <j.sitkovska@gmail.com>
2021-12-08 20:01:10 +0200

    Add project files.

---
+++ CinemaTicketBookingSystem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CinemaTicketBookingSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
